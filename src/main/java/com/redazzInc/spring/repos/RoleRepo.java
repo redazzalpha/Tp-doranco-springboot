cc3ec907009fbfa126e3e075f1cfd268f4141b2d
kyzer <redazzkillah@gmail.com>
2022-03-18 09:58:02 +0100

    initial commit

---
+++ src/main/java/com/redazzInc/spring/repos/RoleRepo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.redazzInc.spring.repos;
+
+import com.redazzInc.spring.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author kyzer
+ */
+public interface RoleRepo extends JpaRepository<Category, Long> {
+    
+}
